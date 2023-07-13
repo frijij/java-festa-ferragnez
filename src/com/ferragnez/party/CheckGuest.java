d9ed699930b14d3e07d310176fe7ab78ec0b10a5
francesca <francipeco36@gmail.com>
2023-07-13 15:20:33 +0200

    file preparation

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,33 @@
+package com.ferragnez.party;
+
+
+import java.util.Scanner;
+
+//Nel programma bisogna:
+//creare e inizializzare l’array contenente i nomi degli invitati
+//chiedere all’utente come si chiama
+//verificare che il nome sia presente nella lista (ATTENZIONE: in Java per confrontare due stringhe non posso
+// usare ==, ma devo usare il metodo equals())
+//lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+//Bonus: se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo
+// stesso programma usando il ciclo while, e viceversa
+//Buon lavoro e buon weekend!
+public class CheckGuest {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //creo l'array con gli invitati
+
+
+        //chiedo il nome all'utente
+
+
+        //ciclo for per verificare se il nome dell'utente è nella lista
+        // uso if per dire di entrare a chi è in lista e di andarsene a chi non c'è
+
+
+        // bonus: uso ciclo while
+
+    }
+}
